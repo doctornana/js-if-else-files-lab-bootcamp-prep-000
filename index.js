43363554c2cabeac86404fe880bf0cd506cb659b
Nasiha Ocasio <ocasin01@gmail.com>
2018-07-28 17:40:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,12 +2,3 @@ var name = "Joe"
 var height = "74"
 
 
-// Don't worry about this
-module.exports = { name, height
-}
-if (name === "Susan") {
-    console.log("The name is correct")
-}
-if (name === "Susan") {
-    console.log("Expected the name to be Susan")
-}
